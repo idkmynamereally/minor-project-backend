aba9ccaa3bfba324d0c46b64b58b67f74ea9317d
Harshit <harshitsaraswat1234@gmail.com>
2024-04-29 14:31:24 +0530

    removed arrays

--- models/projectModel.js
+++ models/projectModel.js
@@ -5,30 +5,22 @@ const projectSchema = mongoose.Schema({
         type: String,
         required: [true, "Please add a project name"]
     },
-    members: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Student'
-        }
-    ],
-    mentors: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Teacher'
-        }
-    ],
-    coordinators: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Teacher'
-        }
-    ],
-    evaluators: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Teacher'
-        }
-    ]
+    members: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student'
+    },
+    mentors: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher'
+    },
+    coordinators: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher'
+    },
+    evaluators: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher'
+    }
 })
 
 module.exports = mongoose.model('Project', projectSchema);
